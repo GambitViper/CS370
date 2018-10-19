f446deb520e58a4423d43f0c862fcf0442d8cb5f
baklund.zachary <baklund.zachary@labiMac-3.local>
2018-10-19 14:41:43 -0500

    Add Reverse functions to change binary to correct order.

--- InstrDecode.c
+++ InstrDecode.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 void HexToBinary(int *, unsigned int);
+void ReverseArray(int []);
+void ReverseArrayHelper(int [], int, int);
 
 int main() {
 
@@ -13,6 +15,7 @@ int main() {
         int *bits;
         bits = (int *) malloc(32 * sizeof(int));
         HexToBinary(bits, hex);
+        ReverseArray(bits);
         for(int i = 0; i < 32; ++i){
             printf("%d", bits[i]);
         }
@@ -26,4 +29,19 @@ void HexToBinary(int *bits, unsigned int hex){
     for(int i = 0; i < 32; ++i){
         bits[i] = (hex >> i) & 1;
     }
+}
+
+void ReverseArray(int *bits){
+  int len = sizeof(&bits) / sizeof(int);
+  int start = 0, end = len - 1;
+  ReverseArrayHelper(bits, start, end);
+}
+
+void ReverseArrayHelper(int *arr, int start, int end){
+  int temp;
+  if(start >= end) return;
+  temp = arr[start];
+  arr[start] = arr[end];
+  arr[end] = temp;
+  ReverseArrayHelper(arr, start + 1, end - 1);
 }
