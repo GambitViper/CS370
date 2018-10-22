1346e45c603385604e40915b8d737df9e95aae1e
baklund.zachary <baklund.zachary@labiMac-3.local>
2018-10-21 20:12:04 -0500

    switch logic to masking

--- InstrDecode.c
+++ InstrDecode.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void HexToBinary(int *, unsigned int);
-void ReverseArray(int *, int);
-void ReverseArrayHelper(int *, int, int);
 unsigned int GetHexValue(unsigned int, int, int);
 struct statistics{
   int insts;
@@ -131,25 +128,4 @@ unsigned int GetHexValue(unsigned int encoded, int hi, int lo){
   result = encoded & mask;
   result = result >> lo;
   return result;
-}
-
-void HexToBinary(int *bits, unsigned int hex){
-    int i;
-    for(i = 0; i < 32; ++i){
-        bits[i] = (hex >> i) & 1;
-    }
-}
-
-void ReverseArray(int *arr, int len){
-  int start = 0, end = len - 1;
-  ReverseArrayHelper(arr, start, end);
-}
-
-void ReverseArrayHelper(int *arr, int start, int end){
-  int temp;
-  if(start >= end) return;
-  temp = arr[start];
-  arr[start] = arr[end];
-  arr[end] = temp;
-  ReverseArrayHelper(arr, start + 1, end - 1);
 }
