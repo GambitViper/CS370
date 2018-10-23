9fdc8bea8231038bf188c7d3efbf2136419a31f5
Zachary Baklund <baklundz2015@gmail.com>
2018-10-23 01:33:16 +0100

    added calculation and formatting for percent values

--- InstrDecode.c
+++ InstrDecode.c
@@ -14,11 +14,13 @@ struct statistics{
   int stores;
   int arith;
 
+  unsigned int branchaddr;
+
   int regreads [32];
   int regwrites [32];
 };
 typedef struct statistics statistics;
-void Dissassemble(unsigned int, statistics *);
+void Dissassemble(unsigned int, unsigned int, statistics *);
 void PrintStatistics(statistics *);
 statistics * Statistics();
 
@@ -36,6 +38,16 @@ int main() {
 
     fp = fopen("trace.txt", "r");
     while (fscanf(fp, "%X %X", &pcaddr, &einstr) != EOF){
+        
+        if(stats->branchaddr != -1){
+          if(pcaddr > (stats->branchaddr + 4)){
+            stats->fwdtaken++;
+          }else if(pcaddr == (stats->branchaddr + 4)){
+            stats->nottaken++;
+          }else{
+            stats->bkwtaken++;
+          }
+        }
 
         // PC address bits
         if(debug == 1){
@@ -51,7 +63,7 @@ int main() {
 
         // Main function dissasembles instructions
         // & Records to statistics struct
-        Dissassemble(einstr, stats);
+        Dissassemble(pcaddr, einstr, stats);
 
         if(debug == 1){
           printf("\n");
@@ -83,6 +95,8 @@ statistics * Statistics(){
   newstat->stores = 0;
   newstat->arith = 0;
 
+  newstat->branchaddr = -1;
+
   int i;
   for(i = 0; i < 32; ++i){
     newstat->regreads[i] = 0;
@@ -92,7 +106,7 @@ statistics * Statistics(){
   return newstat;
 }
 
-void Dissassemble(unsigned int encoded, statistics *stats){
+void Dissassemble(unsigned int pcaddr, unsigned int encoded, statistics *stats){
   unsigned int opcode, rs, rt, rd, shamt, funct, imm, addr;
   opcode = GetHexValue(encoded, 31, 26);
   rs = GetHexValue(encoded, 25, 21);
@@ -110,6 +124,7 @@ void Dissassemble(unsigned int encoded, statistics *stats){
     printf("\tfunct--> %d\n", funct);
   }
 
+  stats->branchaddr = -1;
   stats->insts++;
   
   if(opcode == 0){
@@ -124,19 +139,23 @@ void Dissassemble(unsigned int encoded, statistics *stats){
       stats->regwrites[rd]++;
       stats->regreads[rs]++;
       stats->regreads[rt]++;
+      stats->arith++;
     }else if(funct == 0x0 || (funct >= 0x2 && funct <= 0x3)){
       // R[rd] = R[rt]
       if(debug == 1) printf("\tR[%d] = R[%d]", rd, rt);
       stats->regwrites[rd]++;
       stats->regreads[rt]++;
+      stats->arith++;
     }else if(funct == 0x8){
       // PC = R[rs]
       if(debug == 1) printf("\tPC = R[%d]", rs);
       stats->regreads[rs]++;
+      stats->arith++;
     }
   }else if(opcode <= 3){
     // J-type Instructions
     stats->jtype++;
+    stats->branchaddr = pcaddr;
     addr = GetHexValue(encoded, 25, 0);
     if(debug == 1){
       printf("j-type\n");
@@ -150,13 +169,37 @@ void Dissassemble(unsigned int encoded, statistics *stats){
       printf("i-type\n");
       printf("\timm--> %d\n", imm);
     }
-    if((funct >= 0xa && funct <= 0xd) 
-        || (funct >= 0x23 && funct <= 0x25) 
-        || (funct >= 0x8 && funct <= 0x9)){
+    if((opcode >= 0xa && opcode <= 0xd)
+        || (opcode >= 0x8 && opcode <= 0x9)){
       // R[rt] = R[rs]
       if(debug == 1) printf("\tR[%d] = R[%d] ...", rt, rs);
       stats->regwrites[rt]++;
       stats->regreads[rs]++;
+      stats->arith++;
+    }else if(opcode >= 0x4 && opcode <= 0x5){
+      // Branch beq or bne
+      // if(R[rs](==|!=)R[rt])
+      if(debug == 1) printf("\tBranch if(R[%d](==|!=)R[%d])", rs, rt);
+      stats->regreads[rs]++;
+      stats->regreads[rt]++;
+      stats->branchaddr = pcaddr;
+    }else if(opcode == 0xf){
+      // lui instruction NOT a load
+      if(debug == 1) printf("\t(lui)R[%d] = {imm 16'b0}", rt);
+      stats->regwrites[rt]++;
+      stats->arith++;
+    }else if(opcode >= 0x23 && opcode <= 0x25){
+      // Loads
+      if(debug == 1) printf("\tLoad R[%d] = M[R[%d]]", rt, rs);
+      stats->loads++;
+      stats->regwrites[rt]++;
+      stats->regreads[rs]++;
+    }else if(opcode == 0x28 || opcode == 0x29 || opcode == 0x2b){
+      // Stores
+      if(debug == 1) printf("\tStore M[R[%d] & R[%d]]", rs, rt);
+      stats->stores++;
+      stats->regreads[rs]++;
+      stats->regreads[rt]++;
     }
   }
 }
@@ -177,12 +220,22 @@ void PrintStatistics(statistics *stats){
   printf("r-type: %d\n", stats->rtype);
   printf("i-type: %d\n", stats->itype);
   printf("j-type: %d\n", stats->jtype);
-  printf("fwd-taken: %d\n", stats->fwdtaken);
-  printf("bkw-taken: %d\n", stats->bkwtaken);
-  printf("not-taken: %d\n", stats->nottaken);
-  printf("loads: %d\n", stats->loads);
-  printf("stores: %d\n", stats->stores);
-  printf("arith: %d\n", stats->arith);
+
+  double fwdpct = (double)stats->fwdtaken / stats->insts;
+  double bkwpct = (double)stats->bkwtaken / stats->insts;
+  double notpct = (double)stats->nottaken / stats->insts;
+
+  printf("fwd-taken: %lf\n", (fwdpct * 100));
+  printf("bkw-taken: %lf\n", (bkwpct * 100));
+  printf("not-taken: %lf\n", (notpct * 100));
+
+  double lpct = (double)stats->loads / stats->insts;
+  double spct = (double)stats->stores / stats->insts;
+  double apct = (double)stats->arith / stats->insts;
+
+  printf("loads: %lf\n", (lpct * 100));
+  printf("stores: %lf\n", (spct * 100));
+  printf("arith: %lf\n", (apct * 100));
   int i;
   for(i = 0; i < 32; ++i){
     printf("reg-%d: %d %d\n", i, stats->regreads[i], stats->regwrites[i]);
